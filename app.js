e53b40abf9ce389cf9efbfc955b46449dfecca11
manohar <manumanohar1527@gmail.com>
2020-02-09 15:39:24 -0800

    updated eventlisteners

--- app.js
+++ app.js
@@ -3,14 +3,31 @@
 init();
 let upperUI = document.getElementById('UI');
 
-//Adding the eventListners
-let eventListners = document.addEventListener('keypress', function (event) {
-    key.innerHTML = event.key;
-    keyCode.innerHTML = event.keyCode;
-    keyLocation.innerHTML = event.location;
-    upperUI.innerHTML = event.keyCode;
+//IFFI to display the info text
+(function () {
+    document.getElementById('text').style.display = 'block';
+})();
+
+//setting the info text to hidden when users presses a key
+document.addEventListener('keypress', function () {
+    document.getElementById('text').style.display = 'none';
 });
 
+//calling an eventListener as soon as info text disappers
+
+eventListeners();
+
+
+function eventListeners() {
+    let eventListners = document.addEventListener('keypress', function (event) {
+        key.innerHTML = event.key;
+        keyCode.innerHTML = event.keyCode;
+        keyLocation.innerHTML = event.location;
+        upperUI.innerHTML = event.keyCode;
+    });
+}
+
+//Initialising function
 function init() {
     let key = document.getElementById('key');
     let keyCode = document.getElementById('keyCode');
