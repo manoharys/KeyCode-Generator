69dd4d891c37a798050548319d8107ab42505102
manohar <manumanohar1527@gmail.com>
2020-02-09 15:01:48 -0800

    added js file

---
+++ app.js
@@ -0,0 +1,19 @@
+//variable initilisation
+
+init();
+let upperUI = document.getElementById('UI');
+//console.log(upperUI.value);
+console.log(key.innerHTML);
+console.log(keyCode.innerHTML);
+console.log(keyCode.innerHTML);
+
+
+
+
+
+function init() {
+    let key = document.getElementById('key');
+    let keyCode = document.getElementById('keyCode');
+    let keyLocation = document.getElementById('keyLocation');
+
+}
