a7e495bb1e8c80a0b90ed3397b05371074bace48
manohar <manumanohar1527@gmail.com>
2020-02-09 15:10:59 -0800

    added EventListners

--- app.js
+++ app.js
@@ -2,18 +2,17 @@
 
 init();
 let upperUI = document.getElementById('UI');
-//console.log(upperUI.value);
-console.log(key.innerHTML);
-console.log(keyCode.innerHTML);
-console.log(keyCode.innerHTML);
 
-
-
-
+//Adding the eventListners
+let eventListners = document.addEventListener('keypress', function (event) {
+    key.innerHTML = event.key;
+    keyCode.innerHTML = event.keyCode;
+    keyLocation.innerHTML = event.location;
+    upperUI.innerHTML = event.keyCode;
+});
 
 function init() {
     let key = document.getElementById('key');
     let keyCode = document.getElementById('keyCode');
     let keyLocation = document.getElementById('keyLocation');
-
 }
